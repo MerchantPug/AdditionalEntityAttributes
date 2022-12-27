0c7fbf73649b19faacf5fa847658d5023991fba9
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-28 06:08:16 +1100

    check if player is null

--- Common/src/main/java/de/dafuqs/additionalentityattributes/Support.java
+++ Common/src/main/java/de/dafuqs/additionalentityattributes/Support.java
@@ -6,6 +6,9 @@ import net.minecraft.world.entity.player.Player;
 public class Support {
 	
 	public static float getExperienceMod(Player player) {
+		if(player == null) {
+			return 1.0F;
+		}
 		AttributeInstance attributeInstance = player.getAttribute(AdditionalEntityAttributes.DROPPED_EXPERIENCE);
 		if (attributeInstance == null) {
 			return 1.0F;
