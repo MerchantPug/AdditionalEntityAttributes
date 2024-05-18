c9297c354680ec245fb3a54fa96637104c4134f9
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-19 07:13:01 +1000

    Fix camera with regular scale attribute.

--- src/main/java/de/dafuqs/additionalentityattributes/mixin/client/CameraMixin.java
+++ src/main/java/de/dafuqs/additionalentityattributes/mixin/client/CameraMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 // We run at a lower priority because we clamp values.
 // This is so Pehkui and other mods that may touch this may run without limits.
@@ -15,11 +16,11 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class CameraMixin {
     @Shadow private Entity focusedEntity;
 
-    @ModifyArg(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;clipToSpace(D)D"), index = 0)
-    private double additionalEntityAttributes$updateCameraScaleValue(double desiredCameraDistance) {
+    @ModifyVariable(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;clipToSpace(D)D"), ordinal = 1)
+    private float additionalEntityAttributes$updateCameraScaleValue(float scale) {
         if (this.focusedEntity instanceof LivingEntity living) {
-            return (float) Support.getModelHeight(living, desiredCameraDistance / 4.0F) * 4.0F;
+            return (float) Support.getModelHeight(living, scale);
         }
-        return desiredCameraDistance;
+        return scale;
     }
 }
